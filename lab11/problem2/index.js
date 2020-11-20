eb86371142af3e1f5ec729f3bd6726be5ac8374a
MewCHwcx <en.chayanis@kkumail.com>
2020-11-20 19:54:00 +0700

    del folderextra

--- lab11/problem2/index.js
+++ lab11/problem2/index.js
@@ -4,7 +4,7 @@ var multer = require('multer');
 var upload = multer();
 var app = express();
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('form', {
         name: "Student Form"
     });
@@ -15,17 +15,17 @@ app.set('views', './public');
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(upload.array());
 app.use(express.static('public'));
 
-app.post('/', function(req, res) {
+app.post('/', function (req, res) {
     console.log(req.body);
     var name = req.body.name;
     var company = req.body.company;
     var skill = req.body.skill;
-    res.send(`<b>${name}</b> used these following skills:<b>${skill}</b> while doing internship at <b>${company}</b>` )
+    res.send(`<b>${name}</b> used these following skills:<b>${skill}</b> while doing internship at <b>${company}</b>`)
 })
 
 app.listen(3000);
